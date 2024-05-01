f5e89b54e42f15d40fe7967d497d8c113057acc2
dolphin <fun53@ukr.net>
2024-05-01 22:37:32 +0300

    signup controller v0.2

--- src/app/domain/auth/Signup.ts
+++ src/app/domain/auth/Signup.ts
@@ -2,6 +2,9 @@ import "reflect-metadata";
 import { JsonController, Post, Body } from "routing-controllers";
 import { PrismaClient } from "@prisma/client";
 import { ISignup } from "./Auth.types";
+import { ApiResponse } from "helpers/ApiResponse";
+import { ApiError } from "helpers/ApiError";
+import { hashPassword, random } from "helpers";
 
 const prisma = new PrismaClient();
 const { JWT_SECRET, TOKEN_TTL, APP_URL } = process.env;
@@ -12,14 +15,16 @@ export default class Signup {
   async createUser(@Body() body: ISignup) {
     try {
       const { username, email, password } = body;
-			const user = await prisma.user.findFirst({ where: { email } });
-  if (user) {
-    return new Error("409, Invalid e-mail"); // replace to middleware HttpError
-  }
-			
+      const user = await prisma.user.findFirst({ where: { email } });
+      if (user) {
+        return new ApiError(409, { code: "INVALID_EMAIL", message: "Invalid e-mail" });
+      }
+      const salt = random();
+      const newUser = await prisma.user.create({ data: { username, email, password: hashPassword(salt, password), salt } });
+      return new ApiResponse(true, { email: newUser.email });
     } catch (error) {
       console.log(error);
-      return 400; // how to return STATUS 400???
+      return new ApiError(400, { code: "BAD_REQUEST", message: "Bad Request" });
     }
   }
 }
